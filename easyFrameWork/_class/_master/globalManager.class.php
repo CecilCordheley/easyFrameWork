93dade8af656f10095dd719e035dbaa53921f6ac
CecilCordheley <35634636+CecilCordheley@users.noreply.github.com>
2024-02-07 12:57:29 +0100

    Correction des classes de superglobale

--- easyFrameWork/_class/_master/globalManager.class.php
+++ easyFrameWork/_class/_master/globalManager.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Interface définissant les fonctions de base de la gestion des variables
  */
@@ -8,15 +9,79 @@ interface GlobalManager
     public static function get($key);
 }
 /**
+ * Récupère les informations relatives au Server
+ */
+class ServerInfo
+{
+    private $serverData;
+
+    public function __construct(array $serverData)
+    {
+        $this->serverData = $serverData;
+    }
+
+    public function getServerData($key)
+    {
+        return $this->serverData[$key] ?? null;
+    }
+
+    public function getSelf()
+    {
+        return $this->getServerData("PHP_SELF");
+    }
+
+    public function getScriptName()
+    {
+        return $this->getServerData("SCRIPT_NAME");
+    }
+
+    public function getScriptFilename()
+    {
+        return $this->getServerData("SCRIPT_FILENAME");
+    }
+
+    public function getPath()
+    {
+        return $this->getServerData("PATH_TRANSLATED");
+    }
+
+    public function getRoot()
+    {
+        return $this->getServerData("DOCUMENT_ROOT");
+    }
+
+    public function getRequestFloatTime()
+    {
+        return $this->getServerData("REQUEST_TIME_FLOAT");
+    }
+
+    public function getRacine()
+    {
+        return explode('/', $this->getScriptName())[1] ?? null;
+    }
+
+    public function getRequestTime()
+    {
+        return $this->getServerData("REQUEST_TIME");
+    }
+
+    public function getIpAddress()
+    {
+        return $this->getServerData("REMOTE_ADDR") ?? $this->getServerData("SERVER_ADDR");
+    }
+}
+
+/**
  * Regroupe l'ensemble des variable $_SERVER
+ * @deprecated version
  */
 class Server implements GlobalManager
 {
-/**
- * Retourne toutes les variables server
- * @return array
- */
-    public static function getAll()
+    /**
+     * Retourne toutes les variables server
+     * @return array
+     */
+    public static function getAll(): array
     {
         return $_SERVER;
     }
@@ -91,112 +156,201 @@ class Server implements GlobalManager
 /**
  * Permet de gérer les variables $_GET
  */
-class Request implements GlobalManager
+class Request
 {
-    /**
-     * Retourne la variable GET associé à la clé
-     * @param string $key
-     * @return mixed
-     */
-    public static function get($key)
+    private $getData;
+
+    public function __construct()
     {
-        return $_GET[$key] ?? null;
+        $this->getData = $_GET;
     }
-    /**
-     * Retourne toutes les variables GET
-     * @return mixed
-     */
-    public static function getAll()
+
+    public function getAll(): array
     {
-        return $_GET ?? null;
+        return $this->getData ?? null;
     }
-    /**
-     * Paramètre une variable GET
-     * @param string $key
-     * @param mixed $value
-     */
-    public static function set($key, $value)
+
+    public function hasGetValues(): bool
     {
-        $_GET[$key] = $value;
+        return !empty($this->getData);
+    }
+
+    public function get($key)
+    {
+        if (is_string($key) && array_key_exists($key, $this->getData)) {
+            return $this->getData[$key];
+        } elseif (is_array($key) && count($key) == 2 && isset($this->getData[$key[0]][$key[1]])) {
+            return $this->getData[$key[0]][$key[1]];
+        } else {
+            throw new InvalidArgumentException("Invalid key provided.");
+        }
     }
 }
 /**
  * Permet de gérer les variable $_POST
  */
-class Query implements GlobalManager
+class Query
 {
-    /**
-     * Retourne l'ensemble des variables $_POST
-     * @return array
-     */
-    public static function getAll()
+    private $postData;
+
+    public function __construct()
     {
-        return ["date" => date("Y-m-d"),
-            "values" => $_POST];
+        $this->postData = $_POST;
     }
-    /**
-     * Indique si des variables
-     * @return bool
-     */
-    public static function hasPostValues()
+
+    public function getAll(): array
     {
-        return count($_POST) ?? false;
+        return [
+            "date" => date("Y-m-d"),
+            "values" => $this->postData
+        ];
     }
-    /**
-     * retourne une variable POST
-     * @param string $key
-     * @return mixed
-     */
-    public static function get($key)
+
+    public function hasPostValues(): bool
     {
-        $type = ["string", "array"];
-        if (in_array(gettype($key), $type)) {
-            if (gettype($key) == "string")
-                return $_POST[$key] ?? null;
-            else if (gettype($key) == "array") {
-                return $_POST[$key[0]][$key[1]];
-            }
-        } else
-            throw new Exception("\$key not a valid type should be array or string type " . gettype($key) . " given");
+        return !empty($this->postData);
     }
+
+    public function get($key)
+    {
+        if (is_string($key) && array_key_exists($key, $this->postData)) {
+            return $this->postData[$key];
+        } elseif (is_array($key) && count($key) == 2 && isset($this->postData[$key[0]][$key[1]])) {
+            return $this->postData[$key[0]][$key[1]];
+        } else {
+            throw new InvalidArgumentException("Invalid key provided.");
+        }
+    }
+}
+
+class Session
+{
 }
 /**
  * Permet de gérer les variables $_SESSION lesquelles sont définie soit comme public soit comme privée(cryptée)
+ * @deprecated version
  */
-class sessionVar implements GlobalManager
+class SessionVar implements GlobalManager
 {
     public static function getAll()
     {
         return $_SESSION ?? null;
     }
+
     public static function get($key): mixed
     {
-        if (gettype($key) != "array") {
-            throw new Exception("\$key must be an associate array with 'context' key and 'name' key");
+        if (!is_array($key) || !array_key_exists('context', $key) || !array_key_exists('name', $key)) {
+            throw new Exception("\$key must be an associative array with 'context' and 'name' keys");
         }
+
         $context = $key["context"];
         $_key = $key["name"];
+
         return $_SESSION[$context][$_key] ?? null;
     }
+
     public static function setPublic($key, $value)
     {
-        if (!isset($_SESION["public"])) {
+        if (!isset($_SESSION["public"])) {
             $_SESSION['public'] = [];
         }
+
         $_SESSION['public'][$key] = $value;
     }
+
     public static function getPrivate($key)
     {
-        if (!isset($_SESION["private"][$key])) {
+        if (isset($_SESSION["private"][$key])) {
             return easyFrameWork::decrypt($_SESSION['private'][$key], session_id());
-        } else
+        } else {
             return null;
+        }
     }
+
     public static function setPrivate($key, $value)
     {
-        if (!isset($_SESION["private"])) {
+        if (!isset($_SESSION["private"])) {
             $_SESSION['private'] = [];
         }
+
         $_SESSION['private'][$key] = easyFrameWork::encrypt($value, session_id());
+    }
+}
+class CookieManager
+{
+    public static function getAll()
+    {
+        return $_COOKIE ?? null;
+    }
+
+    public static function get($key): mixed
+    {
+        return $_COOKIE[$key] ?? null;
+    }
+
+    public static function set($key, $value, $expiration = 0, $path = '/', $domain = '', $secure = false, $httponly = false)
+    {
+        setcookie($key, $value, $expiration, $path, $domain, $secure, $httponly);
+    }
+}
+
+class SessionManager
+{
+    const PRIVATE_CONTEXT = "private";
+    const PUBLIC_CONTEXT = "public";
+    public static function getId(): string
+    {
+        return session_id() ?? null;
+    }
+    public static function getAll(): array
+    {
+        return $_SESSION ?? null;
+    }
+    public static function delete(string $key, string $context = self::PUBLIC_CONTEXT): void
+    {
+        if (isset($_SESSION[$context][$key])) {
+            unset($_SESSION[$context][$key]);
+        } else
+            throw new Exception("$key doesn't exist in $context context");
+    }
+    public static function sessionExist(): bool
+    {
+        return session_id() != null;
+    }
+    public static function clean(): void
+    {
+        if (self::sessionExist())
+            session_destroy();
+    }
+    public static function get(string $key, string $context = self::PUBLIC_CONTEXT): mixed
+    {
+        if (self::sessionExist())
+            return $_SESSION[$context][$key] ?? null;
+        else
+            throw new Exception("Pas de session activée");
+    }
+    private static function setPrivate($key, $value): void
+    {
+        if (!isset($_SESSION["private"])) {
+            $_SESSION['private'] = [];
+        }
+
+        $_SESSION['private'][$key] = easyFrameWork::encrypt($value, session_id());
+    }
+    private static function setPublic($key, $value): void
+    {
+        if (!isset($_SESSION["public"])) {
+            $_SESSION['public'] = [];
+        }
+
+        $_SESSION['public'][$key] = $value;
+    }
+    public static function set($key, $value, $context = self::PUBLIC_CONTEXT): void
+    {
+        if ($context == self::PRIVATE_CONTEXT) {
+            self::setPrivate($key, $value);
+        } else {
+            self::setPublic($key, $value);
+        }
     }
 }
